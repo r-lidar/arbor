df3a09e77910448db3077ecd6612353234a28245
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2025-12-07 07:34:46 -0500

    fix

--- R/qsm_simplify.R
+++ R/qsm_simplify.R
@@ -1,5 +1,6 @@
 qsm_simplify <- function(qsm, max_length = 0.3)
 {
+  qsm$cyl_length = sqrt((qsm$startX - qsm$endX)^2 + (qsm$startY- qsm$endY)^2 + (qsm$startZ - qsm$endZ)^2)
   qsm = qsm_simplify_cpp(qsm, max_length)
   qsm = qsm_topology(qsm)
   qsm = qsm_architecture(qsm)
