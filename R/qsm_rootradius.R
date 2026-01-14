e69040073bbfb4343b2ff75f3d868435ff6f7be2
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2026-01-14 07:32:02 -0500

    R CMD check

--- R/qsm_rootradius.R
+++ R/qsm_rootradius.R
@@ -113,7 +113,7 @@ measure_cylinder_radius = function(pc, qsm, id)
 
 # Griese, N., Ritzert, M. & Nölke, N. A large dataset of labelled single tree point clouds, QSMs and
 # tree graphs. Sci Data 12, 1953 (2025). https://doi.org/10.1038/s41597-025-06421-7
-H_vs_DBH_allometry = function(dbh) { 36.03*(1-exp(-0.05*DBH))^1.1 }
+H_vs_DBH_allometry = function(dbh) { 36.03*(1-exp(-0.05*dbh))^1.1 }
 DBH_vs_H_allometry = function(H) { DBH = -1/0.05*log(1-(H/36.03)^(1/1.1)) ; DBH/100 }
 
 
