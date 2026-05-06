8a803f075ba1a26f2e3ce1b667cd126c2e63ae0c
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2026-05-06 12:23:59 -0400

    Doc

--- R/arbor_parameters.R
+++ R/arbor_parameters.R
@@ -1,8 +1,12 @@
 #' Parameters
+#'
+#' Default is suitable for most dataset. Occlusion is suitable for lower quality datasets
 #' @rdname parameters
 #' @name parameters
 #' @export
 arbor_parameters_default = list()
 
 #' @export
+#' @rdname parameters
+#' @name parameters
 arbor_parameters_occlusion = list()
