bb7d81c09d401b7dc56262fcd441409bd3ccfe4e
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2026-05-13 15:35:53 -0400

    15 cm

--- R/qsm_subset.R
+++ R/qsm_subset.R
@@ -1,18 +1,18 @@
 # @file qsm_subset.R
 # Project: Arbor
-# 
+#
 # Copyright (C) 2026 Jean-Romain Roussel (r-lidar) <info @ r-lidar.com>
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -94,7 +94,7 @@ qsf_merchantable = function(qsf, merchantable_radius = 0.045)
 }
 
 #' @export
-#' @param stump_height The height to remove from the root. Default 25 cm
+#' @param stump_height The height to remove from the root. Default 15 cm
 #' @rdname qsm_subset
 qsm_nostump = function(qs, stump_height = 0.15)
 {
