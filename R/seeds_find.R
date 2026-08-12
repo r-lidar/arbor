28710aa1d90975e78c0f64bdfdc1dc1c0ab423a8
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2026-08-12 08:52:37 -0400

    find seed default parameters

--- R/seeds_find.R
+++ R/seeds_find.R
@@ -1,18 +1,18 @@
 # @file seeds_find.R
 # Project: Arbor
-# 
+#
 # Copyright (C) 2026 Jean-Romain Roussel (r-lidar) <info @ r-lidar.com>
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -26,7 +26,7 @@
 #' @param params list See \link{parameters}.
 #' @export
 #' @md
-find_seeds <- function(las, params)
+find_seeds <- function(las, params = arbor_parameters_default)
 {
   stop_if_not_tls(las)
   cloud = las@data ; cloud[["treeID"]] = -1L # Allocate memory because C++ assumes memory allocated
